3316f3938d4835d829367e19b50b8b107072cd7a
justreddittest <justtestrestoration@users.noreply.github.com>
2016-05-12 20:10:33 +0000

    Create init.lua
    in the effort to create a land with scarcity similar to just test

---
+++ init.lua
@@ -0,0 +1,34 @@
+-- See README.md for licensing and other information.
+
+minetest.register_craft({
+	output = 'default:cobble 9',
+	recipe = {
+		{"default:dirt", "default:dirt", "default:dirt"},
+		{"default:dirt", "default:dirt", "default:dirt"},
+		{"default:dirt", "default:dirt", "default:dirt"},
+	},
+})
+minetest.register_craft({
+	output = 'default:cobble 9',
+	recipe = {
+		{"default:desert_cobble", "default:desert_cobble", "default:desert_cobble"},
+		{"default:desert_cobble", "default:desert_cobble", "default:desert_cobble"},
+		{"default:desert_cobble", "default:desert_cobble", "default:desert_cobble"},
+	},
+})
+minetest.register_craft({
+	output = 'default:cobble 9',
+	recipe = {
+		{"default:desert_sand", "default:desert_sand", "default:desert_sand"},
+		{"default:desert_sand", "default:desert_sand", "default:desert_sand"},
+		{"default:desert_sand", "default:desert_sand", "default:desert_sand"},
+	},
+})
+minetest.register_craft({
+	output = 'default:cobble 9',
+	recipe = {
+		{"default:sand", "default:sand", "default:sand"},
+		{"default:sand", "default:sand", "default:sand"},
+		{"default:sand", "default:sand", "default:sand"},
+	},
+})
