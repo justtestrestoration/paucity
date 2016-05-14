92b6083c28d6adf4b5f882683efd0dd1f0c886a5
justreddittest <justtestrestoration@users.noreply.github.com>
2016-05-14 11:29:20 +0000

    final piece to upgrade paucity
    though the final piece should be to update the readme.md giving a better explanation of the mod

--- init.lua
+++ init.lua
@@ -32,3 +32,111 @@ minetest.register_craft({
 		{"default:sand", "default:sand", "default:sand"},
 	},
 })
+
+-- -----------------------------------
+-- -----------------------------------
+-- -----------------------------------
+
+--emptymoney
+minetest.register_node("paucity:emptymoney", {
+	description = "emptymoney",
+	tiles = {"emptymoney.png"},
+	groups = {choppy=2,dig_immediate=2},
+})
+
+--ironmoney
+minetest.register_node("paucity:ironmoney", {
+	description = "ironmoney",
+	tiles = {"ironmoney.png"},
+	groups = {choppy=2,dig_immediate=2},
+})
+
+---------------
+-- <emptymoney>
+---------------
+
+--how to craft emptymoney with default:dirt
+minetest.register_craft({
+	output = 'paucity:emptymoney',
+	recipe = {
+		{"default:dirt","default:dirt","default:dirt"},
+		{"default:dirt","default:dirt","default:dirt"},
+		{"","",""},
+	}
+})
+
+--how to craft emptymoney with default:desert_cobble
+minetest.register_craft({
+	output = 'paucity:emptymoney',
+	recipe = {
+		{"default:desert_cobble","default:desert_cobble","default:desert_cobble"},
+		{"default:desert_cobble","default:desert_cobble","default:desert_cobble"},
+		{"","",""},
+	}
+})
+
+--how to craft emptymoney with default:desert_sand
+minetest.register_craft({
+	output = 'paucity:emptymoney',
+	recipe = {
+		{"default:desert_sand","default:desert_sand","default:desert_sand"},
+		{"default:desert_sand","default:desert_sand","default:desert_sand"},
+		{"","",""},
+	}
+})
+
+--how to craft emptymoney with default:sand
+minetest.register_craft({
+	output = 'paucity:emptymoney',
+	recipe = {
+		{"default:sand","default:sand","default:sand"},
+		{"default:sand","default:sand","default:sand"},
+		{"","",""},
+	}
+})
+
+----------------
+-- </emptymoney>
+----------------
+
+
+--how to craft ironmoney
+minetest.register_craft({
+	output = 'paucity:ironmoney',
+	recipe = {
+		{"paucity:emptymoney","paucity:emptymoney","paucity:emptymoney"},
+		{"paucity:emptymoney","paucity:emptymoney","paucity:emptymoney"},
+		{"","",""},
+	}
+})
+
+
+--get 1 iron lump for 2 ironmoney
+minetest.register_craft({
+	output = 'default:iron_lump',
+	recipe = {
+		{"paucity:ironmoney","paucity:ironmoney",""},
+		{"","",""},
+		{"","",""},
+	}
+})
+
+--get 1 coal for 1 ironmoney
+minetest.register_craft({
+	output = 'default:coal_lump',
+	recipe = {
+		{"paucity:ironmoney","",""},
+		{"","",""},
+		{"","",""},
+	}
+})
+
+--get 1 default:tree
+minetest.register_craft({
+	output = 'default:tree',
+	recipe = {
+		{"paucity:emptymoney","paucity:ironmoney",""},
+		{"","",""},
+		{"","",""},
+	}
+})
